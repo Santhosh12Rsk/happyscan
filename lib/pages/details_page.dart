3ea85a70bdaf0975770f7d2003e9e6b82456afa0
Srikanth13Reddy <srikanthy@apptomate.co>
2021-11-14 17:42:35 +0530

    Saving in local

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -1,10 +1,8 @@
-import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'dart:io';
-import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import 'package:open_file/open_file.dart';
 import 'dart:io' as io;
 
